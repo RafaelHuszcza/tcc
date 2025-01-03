a10be329e2f2fd5a39ee7a98af229c62d40ed499
rafaelhuszcza <rafaelhuszcza@gmail.com>
2025-01-03 14:51:35 -0300

    fix: output

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    domains: ['raw.githubusercontent.com'],
-  },
+  output: 'standalone',
   reactStrictMode: false,
 };
 

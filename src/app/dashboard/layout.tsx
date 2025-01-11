53bd933536b200d0edafc8d2b0ff9b49c2ab08aa
rafaelhuszcza <rafaelhuszcza@gmail.com>
2025-01-11 19:58:25 -0300

    feat: CD implementation

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -8,9 +8,18 @@ export default function DashboardLayout({
   children: React.ReactNode
 }) {
   return (
-    <SidebarProvider>
-      <AppSidebar />
-      <SidebarInset>{children}</SidebarInset>
-    </SidebarProvider>
+    <>
+      <SidebarProvider>
+        <AppSidebar />
+        <SidebarInset>{children}</SidebarInset>
+      </SidebarProvider>
+      <p
+        className={
+          'absolute bottom-2 right-3 text-center text-xs text-gray-400'
+        }
+      >
+        1.0.3
+      </p>
+    </>
   )
 }
